53722b5eeaa1fd37938a5ce96cc87136f7f51d71
Pedro Luis Pérez Collado <plpcollado93@gmail.com>
2025-11-21 19:04:50 -0600

    fix(lfs): Force Git LFS to use HTTPS and disable SSH for Streamlit Cloud

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Force LFS to use HTTPS instead of SSH
+git config --global lfs.url "https://github.com/plpcollado/mapa_alcaldias_v1.git/info/lfs"
+git config --global url."https://github.com/".insteadOf git@github.com:
 git lfs install
 git lfs pull
